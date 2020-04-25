aa196d6cebde981880d3489fed1f92282291da03
madabalacean1 <mada_balacean@yahoo.com>
2020-04-25 21:38:50 +0000

    generate the required pages and services

--- mobdev_ca2/src/app/pages/characters/characters.page.ts
+++ mobdev_ca2/src/app/pages/characters/characters.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
 
 @Component({
-  selector: 'app-characters',
-  templateUrl: './characters.page.html',
-  styleUrls: ['./characters.page.scss'],
+    selector: 'app-characters',
+    templateUrl: './characters.page.html',
+    styleUrls: ['./characters.page.scss'],
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+    characters: Observable<any>;
 
-  ngOnInit() {
-  }
+    constructor(private router: Router, private api: ApiService) { }
 
+    ngOnInit() {
+        this.characters = this.api.getCharacters();
+        this.characters.subscribe(data => {
+            console.log('my data: ', data);
+        });
+
+    }
+    openDetails(character) {
+        let characterId = character.character_id;
+        this.router.navigateByUrl(`/tabs/characters/${characterId}`);
+    }
 }
