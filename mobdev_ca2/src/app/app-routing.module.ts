e68b9f5dcb150c9d28022f5e58a635ed00d605bc
madabalacean1 <mada_balacean@yahoo.com>
2020-04-26 23:09:38 +0000

    change color

--- mobdev_ca2/src/app/app-routing.module.ts
+++ mobdev_ca2/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
-  { path: loadChildren: './details/details.module#DetailsPageModule' };
+ // { path: loadChildren: './details/details.module#DetailsPageModule' };
 
 @NgModule({
   imports: [
